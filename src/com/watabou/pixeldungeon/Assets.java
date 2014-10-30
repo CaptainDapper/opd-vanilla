3e00c98f14999e944073a7ff63e75fc6e627ee72
sloanr333 <sloan@lanimals.com>
2014-10-30 02:43:58 -0700

    0.3.2b

--- src/com/watabou/pixeldungeon/Assets.java
+++ src/com/watabou/pixeldungeon/Assets.java
@@ -17,144 +17,15 @@
  */
 package com.watabou.pixeldungeon;
 
-public class Assets {
+import com.opd.openpixeldungeon.DefaultAssets;
 
-	public static final String ARCS_BG		= "vanilla/arcs1.png";
-	public static final String ARCS_FG		= "vanilla/arcs2.png";
-	public static final String DASHBOARD	= "vanilla/dashboard.png";
-	
-	public static final String BANNERS	= "vanilla/banners.png";
-	public static final String BADGES	= "vanilla/badges.png";
-	public static final String AMULET	= "vanilla/amulet.png";
-	
-	public static final String CHROME	= "vanilla/chrome.png";
-	public static final String ICONS	= "vanilla/icons.png";
-	public static final String STATUS	= "vanilla/status_pane.png";
-	public static final String HP_BAR	= "vanilla/hp_bar.png";
-	public static final String XP_BAR	= "vanilla/exp_bar.png";
-	public static final String TOOLBAR	= "vanilla/toolbar.png";
-	
-	public static final String WARRIOR	= "vanilla/warrior.png";
-	public static final String MAGE		= "vanilla/mage.png";
-	public static final String ROGUE	= "vanilla/rogue.png";
-	public static final String HUNTRESS	= "vanilla/ranger.png";
+public class Assets extends DefaultAssets {
 	public static final String AVATARS	= "vanilla/avatars.png";
-	public static final String PET		= "vanilla/pet.png";
-	
-	public static final String SURFACE	= "vanilla/surface.png";
-	
-	public static final String FIREBALL		= "vanilla/fireball.png";
-	public static final String SPECKS		= "vanilla/specks.png";
-	public static final String EFFECTS		= "vanilla/effects.png";
-	
-	public static final String RAT		= "vanilla/rat.png";
-	public static final String GNOLL	= "vanilla/gnoll.png";
-	public static final String CRAB		= "vanilla/crab.png";
-	public static final String GOO		= "vanilla/goo.png";
-	public static final String SWARM	= "vanilla/swarm.png";
-	public static final String SKELETON	= "vanilla/skeleton.png";
-	public static final String SHAMAN	= "vanilla/shaman.png";
-	public static final String THIEF	= "vanilla/thief.png";
-	public static final String TENGU	= "vanilla/tengu.png";
-	public static final String SHEEP	= "vanilla/sheep.png";
-	public static final String KEEPER	= "vanilla/shopkeeper.png";
-	public static final String BAT		= "vanilla/bat.png";
-	public static final String BRUTE	= "vanilla/brute.png";
-	public static final String SPINNER	= "vanilla/spinner.png";
-	public static final String DM300	= "vanilla/dm300.png";
-	public static final String WRAITH	= "vanilla/wraith.png";
-	public static final String ELEMENTAL= "vanilla/elemental.png";
-	public static final String MONK		= "vanilla/monk.png";
-	public static final String WARLOCK	= "vanilla/warlock.png";
-	public static final String GOLEM	= "vanilla/golem.png";
-	public static final String UNDEAD	= "vanilla/undead.png";
-	public static final String KING		= "vanilla/king.png";
-	public static final String STATUE	= "vanilla/statue.png";
-	public static final String PIRANHA	= "vanilla/piranha.png";
-	public static final String EYE		= "vanilla/eye.png";
-	public static final String SUCCUBUS	= "vanilla/succubus.png";
-	public static final String SCORPIO	= "vanilla/scorpio.png";
-	public static final String ROTTING	= "vanilla/rotting_fist.png";
-	public static final String BURNING	= "vanilla/burning_fist.png";
-	public static final String YOG		= "vanilla/yog.png";
-	public static final String LARVA	= "vanilla/larva.png";
-	public static final String GHOST	= "vanilla/ghost.png";
-	public static final String MAKER	= "vanilla/wandmaker.png";
-	public static final String TROLL	= "vanilla/blacksmith.png";
-	public static final String IMP		= "vanilla/demon.png";
-	public static final String RATKING	= "vanilla/ratking.png";
-	
-	public static final String ITEMS	= "vanilla/items.png";
-	public static final String PLANTS	= "vanilla/plants.png";
-	
-	public static final String TILES_SEWERS	= "vanilla/tiles0.png";
-	public static final String TILES_PRISON	= "vanilla/tiles1.png";
-	public static final String TILES_CAVES	= "vanilla/tiles2.png";
-	public static final String TILES_CITY	= "vanilla/tiles3.png";
-	public static final String TILES_HALLS	= "vanilla/tiles4.png";
-	
-	public static final String WATER_SEWERS	= "vanilla/water0.png";
-	public static final String WATER_PRISON	= "vanilla/water1.png";
-	public static final String WATER_CAVES	= "vanilla/water2.png";
-	public static final String WATER_CITY	= "vanilla/water3.png";
-	public static final String WATER_HALLS	= "vanilla/water4.png";
-	
+	public static final String BADGES	= "vanilla/badges.png";
 	public static final String BUFFS_SMALL	= "vanilla/buffs.png";
+	public static final String ITEMS	= "vanilla/items.png";
 	public static final String BUFFS_LARGE	= "vanilla/large_buffs.png";
-	public static final String SPELL_ICONS	= "vanilla/spell_icons.png";
-	
-	public static final String FONTS1X	= "vanilla/font1x.png";
-	public static final String FONTS15X	= "vanilla/font15x.png";
-	public static final String FONTS2X	= "vanilla/font2x.png";
-	public static final String FONTS25X	= "vanilla/font25x.png";
-	public static final String FONTS3X	= "vanilla/font3x.png";
-	
-	public static final String THEME	= "vanilla/theme.mp3";
-	public static final String TUNE		= "vanilla/game.mp3";
-	public static final String HAPPY	= "vanilla/surface.mp3";
-	
-	public static final String SND_CLICK	= "vanilla/snd_click.mp3";
-	public static final String SND_BADGE	= "vanilla/snd_badge.mp3";
-	public static final String SND_GOLD		= "vanilla/snd_gold.mp3";
-	
-	public static final String SND_OPEN		= "vanilla/snd_door_open.mp3";
-	public static final String SND_UNLOCK	= "vanilla/snd_unlock.mp3";
-	public static final String SND_ITEM		= "vanilla/snd_item.mp3";
-	public static final String SND_DEWDROP	= "vanilla/snd_dewdrop.mp3";
-	public static final String SND_HIT		= "vanilla/snd_hit.mp3";
-	public static final String SND_MISS		= "vanilla/snd_miss.mp3";
-	public static final String SND_STEP		= "vanilla/snd_step.mp3";
-	public static final String SND_WATER	= "vanilla/snd_water.mp3";
-	public static final String SND_DESCEND	= "vanilla/snd_descend.mp3";
-	public static final String SND_EAT		= "vanilla/snd_eat.mp3";
-	public static final String SND_READ		= "vanilla/snd_read.mp3";
-	public static final String SND_LULLABY	= "vanilla/snd_lullaby.mp3";
-	public static final String SND_DRINK	= "vanilla/snd_drink.mp3";
-	public static final String SND_SHATTER	= "vanilla/snd_shatter.mp3";
-	public static final String SND_ZAP		= "vanilla/snd_zap.mp3";
-	public static final String SND_LIGHTNING= "vanilla/snd_lightning.mp3";
-	public static final String SND_LEVELUP	= "vanilla/snd_levelup.mp3";
-	public static final String SND_DEATH	= "vanilla/snd_death.mp3";
-	public static final String SND_CHALLENGE= "vanilla/snd_challenge.mp3";
-	public static final String SND_CURSED	= "vanilla/snd_cursed.mp3";
-	public static final String SND_TRAP		= "vanilla/snd_trap.mp3";
-	public static final String SND_EVOKE	= "vanilla/snd_evoke.mp3";
-	public static final String SND_TOMB		= "vanilla/snd_tomb.mp3";
-	public static final String SND_ALERT	= "vanilla/snd_alert.mp3";
-	public static final String SND_MELD		= "vanilla/snd_meld.mp3";
-	public static final String SND_BOSS		= "vanilla/snd_boss.mp3";
-	public static final String SND_BLAST	= "vanilla/snd_blast.mp3";
-	public static final String SND_PLANT	= "vanilla/snd_plant.mp3";
-	public static final String SND_RAY		= "vanilla/snd_ray.mp3";
-	public static final String SND_BEACON	= "vanilla/snd_beacon.mp3";
-	public static final String SND_TELEPORT	= "vanilla/snd_teleport.mp3";
-	public static final String SND_CHARMS	= "vanilla/snd_charms.mp3";
-	public static final String SND_MASTERY	= "vanilla/snd_mastery.mp3";
-	public static final String SND_PUFF		= "vanilla/snd_puff.mp3";
-	public static final String SND_ROCKS	= "vanilla/snd_rocks.mp3";
-	public static final String SND_BURNING	= "vanilla/snd_burning.mp3";
-	public static final String SND_FALLING	= "vanilla/snd_falling.mp3";
-	public static final String SND_GHOST	= "vanilla/snd_ghost.mp3";
-	public static final String SND_SECRET	= "vanilla/snd_secret.mp3";
-	public static final String SND_BONES	= "vanilla/snd_bones.mp3";
+	public static final String STATUS	= "vanilla/status_pane.png";
+	public static final String TILES_HALLS	= "vanilla/tiles4.png";
+	public static final String WARRIOR	= "vanilla/warrior.png";
 }
